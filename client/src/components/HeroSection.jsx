4944e6687c3fd136ea92ffd691d1769c00d12f61
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-02-02 02:18:12 +0530

    Created the utility function, crafted the hero-section

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import hero_section_desk from '../assets/hero-section-desktop.mp4';
+import hero_section_mob from '../assets/hero-section-mobile.mp4';
+import { isMobile } from '../utils/UtilityFunction';
+import { LuMouse } from "react-icons/lu";
+
+const HeroSection = () => {
+  return (
+    <div className='w-screen h-screen relative'>
+        <video 
+            src={isMobile() ? hero_section_mob : hero_section_desk} 
+            autoPlay 
+            loop 
+            muted 
+            className='w-full h-full object-cover'
+        />
+    
+        <div className='absolute bottom-10 left-1/2 text-gray-50 opacity-75 text-3xl animate-bounce'>
+            <LuMouse />
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ client/src/utils/UtilityFunction.js
@@ -0,0 +1,3 @@
+export const isMobile =() => {
+    if(window.innerWidth <= 800) return true;
+}
