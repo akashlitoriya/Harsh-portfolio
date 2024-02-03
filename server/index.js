1cff7578184bd4e67d478df52e5104d6b2d13888
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-02-03 14:48:37 +0530

    Server starter code added

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config();
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
