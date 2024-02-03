31089f44e56c72073cd2603b3fbf54798d60719e
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-02-03 23:45:02 +0530

    Added horizontal scroll logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import HeroSection from '../components/HeroSection'
 import Contact from '../components/Contact'
+import WorkExperience from "../components/WorkExperience"
 const Home = () => {
+
+  const container = useRef();
+  
+  const handleHorizontalScroll = (e) =>{
+    if(e.deltaY > 0){
+      container.current.scrollBy({left: container.current.clientWidth, behavior: 'smooth'});
+    }else{
+      container.current.scrollBy({left: -container.current.clientWidth, behavior:'smooth'})
+    }
+    
+  }
+
   return (
-    <div className='w-screen h-screen overflow-x-hidden'>
+    <div ref={container}
+      onWheel={handleHorizontalScroll}
+      className='box h-screen overflow-y-hidden overflow-x-scroll scroll-smooth grid grid-flow-col auto-cols-auto snap-x snap-mandatory'>
         <HeroSection />
+        <WorkExperience />
         <Contact />
     </div>
   )
