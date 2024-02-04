2801b7d0e87d80f126fb18ab56e25fa59859a0ff
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-02-04 13:31:25 +0530

    Added cloudinary configurations

--- server/index.js
+++ server/index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const dotenv = require('dotenv');
+//importing configs
+const {cloudinaryConnect} = require('./configs/cloudinary');
+const connectDB = require('./configs/database');
+const fileupload = require('express-fileupload');
+//importing routes
+const reviewRoutes = require('./routes/Review');
 
 const app = express();
+
+//body parser
+app.use(express.json());
 
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 
-const connectDB = require('./configs/database');
+//adding server files
+app.use(
+    fileupload({
+        useTempFiles: true,
+        tempFileDir: 'temp/',
+    })
+)
 
+
+cloudinaryConnect();
 connectDB();
+
+
+//routes
+app.use('/api/v1/review', reviewRoutes);
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
